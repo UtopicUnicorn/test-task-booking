d94b7e88cbc50c0bf2c0e3746f0b3c9c9a0b6b3d
UtopicUnicorn <medvedevtimofei@gmail.com>
2024-01-22 19:09:38 +0700

    added apiReducer to root

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import { bookingFormReducer } from './booking-form/booking-form.reducer';
+import { apiImagesReducer } from './images/images.reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers({
-  bookingForm: bookingFormReducer
+  bookingForm: bookingFormReducer,
+  apiImages: apiImagesReducer
 });
 
 export default rootReducer;
